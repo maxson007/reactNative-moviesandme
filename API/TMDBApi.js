e63875e681f2bfa0e1f7efc1cdd255c1f5f152ec
Daniel Aymane <danielayman@gmail.com>
2019-09-22 16:07:49 +0200

    Maîtrisez les bases de React Native

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -1,7 +1,7 @@
-const API_TOKEN = "XXXXXXXXXXXXXdafafafXXXXXfafa9àiàççàjfijéiXXX56cae6b4fb9179svsvsvsvzvzrvzrvzkmpkma990I9hjknd66f27dc4c7f04403a";
+const API_TOKEN = "56cae6b4fb9179d66f27dc4c7f04403a";
 
-export function getFilmsFromApiWithSearchedText (text) {
-    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text
+export function getFilmsFromApiWithSearchedText (text, page) {
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + "&page=" + page
     return fetch(url)
         .then((response) => response.json())
         .catch((error) => console.error(error))
--- Components/Search.js
+++ Components/Search.js
@@ -1,5 +1,7 @@
+// Components/Search.js
+
 import React from 'react'
-import { StyleSheet, View, TextInput, Button, FlatList , ActivityIndicator } from 'react-native'
+import { StyleSheet, View, TextInput, Button, Text, FlatList, ActivityIndicator } from 'react-native'
 import FilmItem from './FilmItem'
 import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi'
 
@@ -7,23 +9,41 @@ class Search extends React.Component {
 
     constructor(props) {
         super(props)
-        this.searchedText = "" // Initialisation de notre donnée searchedText en dehors du state
+        this.searchedText = ""
+        this.page = 0
+        this.totalPages = 0
         this.state = {
             films: [],
-            isLoading: false // Par défaut à false car il n'y a pas de chargement tant qu'on ne lance pas de recherche
+            isLoading: false
         }
     }
 
     _loadFilms() {
         if (this.searchedText.length > 0) {
-            this.setState({ isLoading: true }) // Lancement du chargement
-            getFilmsFromApiWithSearchedText(this.searchedText).then(data => {
+            this.setState({ isLoading: true })
+            getFilmsFromApiWithSearchedText(this.searchedText, this.page+1).then(data => {
+                this.page = data.page
+                this.totalPages = data.total_pages
                 this.setState({
-                    films: data.results,
-                    isLoading: false // Arrêt du chargement
+                    films: [ ...this.state.films, ...data.results ],
+                    isLoading: false
                 })
             })
         }
+    }
+
+    _searchTextInputChanged(text) {
+        this.searchedText = text
+    }
+
+    _searchFilms() {
+        this.page = 0
+        this.totalPages = 0
+        this.setState({
+            films: [],
+        }, () => {
+            this._loadFilms()
+        })
     }
 
     _displayLoading() {
@@ -31,31 +51,31 @@ class Search extends React.Component {
             return (
                 <View style={styles.loading_container}>
                     <ActivityIndicator size='large' />
-                    {/* Le component ActivityIndicator possède une propriété size pour définir la taille du visuel de chargement : small ou large. Par défaut size vaut small, on met donc large pour que le chargement soit bien visible */}
                 </View>
             )
         }
     }
 
-    _searchTextInputChanged(text) {
-        this.searchedText = text // Modification du texte recherché à chaque saisie de texte, sans passer par le setState comme avant
-    }
-
     render() {
-        console.log("RENDER")
         return (
             <View style={styles.main_container}>
                 <TextInput
                     style={styles.textinput}
                     placeholder='Titre du film'
                     onChangeText={(text) => this._searchTextInputChanged(text)}
-                    onSubmitEditing={() => this._loadFilms()}
+                    onSubmitEditing={() => this._searchFilms()}
                 />
-                <Button title='Rechercher' onPress={() => this._loadFilms()}/>
+                <Button title='Rechercher' onPress={() => this._searchFilms()}/>
                 <FlatList
                     data={this.state.films}
                     keyExtractor={(item) => item.id.toString()}
                     renderItem={({item}) => <FilmItem film={item}/>}
+                    onEndReachedThreshold={0.5}
+                    onEndReached={() => {
+                        if (this.page < this.totalPages) {
+                            this._loadFilms()
+                        }
+                    }}
                 />
                 {this._displayLoading()}
             </View>
