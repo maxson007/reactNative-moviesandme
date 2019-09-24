3b15e1a6880d0a13bdea43fd8e02ad78c99442ba
Daniel Aymane <danielayman@gmail.com>
2019-09-24 22:22:40 +0200

    film vieaw detail

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
-import React from 'react';
-import Search from './Components/Search'
-export default function App() {
-  return (
-      <Search/>
-  );
+import React from 'react'
+import Navigation from './Navigation/Navigation'
+
+export default class App extends React.Component {
+  render() {
+    return (
+        <Navigation/>
+    )
+  }
 }
--- Components/Search.js
+++ Components/Search.js
@@ -56,7 +56,13 @@ class Search extends React.Component {
         }
     }
 
+    _displayDetailForFilm = (idFilm) => {
+        console.log("Display film with id " + idFilm)
+        this.props.navigation.navigate("FilmDetail",  { idFilm: idFilm })
+    }
+
     render() {
+        //console.log(this.props)
         return (
             <View style={styles.main_container}>
                 <TextInput
@@ -69,7 +75,7 @@ class Search extends React.Component {
                 <FlatList
                     data={this.state.films}
                     keyExtractor={(item) => item.id.toString()}
-                    renderItem={({item}) => <FilmItem film={item}/>}
+                    renderItem={({item}) => <FilmItem film={item} displayDetailForFilm={this._displayDetailForFilm} />}
                     onEndReachedThreshold={0.5}
                     onEndReached={() => {
                         if (this.page < this.totalPages) {
@@ -84,10 +90,6 @@ class Search extends React.Component {
 }
 
 const styles = StyleSheet.create({
-    main_container: {
-        flex: 1,
-        marginTop: 20
-    },
     textinput: {
         marginLeft: 5,
         marginRight: 5,
